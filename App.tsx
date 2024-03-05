802a5481633560970afa99f69863aeb836ef03c9
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-03-05 07:35:46 +0700

    flex box

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Image, Button, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, Image, Button, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
 import React from 'react';
 
 declare function alert(message?: any): void;
@@ -7,27 +7,38 @@ const sayHai = () => {
 };
 
 const App = () => {
+
   return (
-    <View>
-      <Text style={style.tulisan}>
-        <Text style={style.italic}> Selamat datang</Text> di
-        aplikasi pertama saya
-      </Text>
-      <Image
-        source={require('./assets/deadpool.png')}
-        style={style.image}
-      />
-      <Button title="Punch Me" onPress={sayHai} />
-      <TouchableOpacity onPress={sayHai} style={style.touchButton}>
-        <Text style={style.textTouchButton} >PUNCH ME ALSO</Text>
-      </TouchableOpacity>
-    </View>
+    <ScrollView>
+      <View>
+        <Text style={style.tulisan}>
+          <Text style={style.italic}> Selamat datang</Text> di
+          aplikasi pertama saya
+        </Text>
+        <Image
+          source={require('./assets/deadpool.png')}
+          style={style.image}
+        />
+        <Button title="Punch Me" onPress={sayHai} />
+        <TouchableOpacity onPress={sayHai} style={style.touchButton}>
+          <Text style={style.textTouchButton} >PUNCH ME ALSO</Text>
+        </TouchableOpacity>
+
+        <View style={style.parentFlex} >
+          <Text style={style.boxYellow} >1</Text>
+          <Text style={style.boxGreen} >2</Text>
+          <Text style={style.boxRed} >3</Text>
+        </View>
+
+      </View>
+    </ScrollView>
   );
 };
 
 export default App;
 
 const style = StyleSheet.create({
+  // pengenalan style
   tulisan: {
     fontSize: 20
   },
@@ -39,6 +50,8 @@ const style = StyleSheet.create({
     width: 350,
     height: 400
   },
+
+  // button
   touchButton: {
     backgroundColor: 'red',
     padding: 10
@@ -47,5 +60,28 @@ const style = StyleSheet.create({
     color: 'white',
     textAlign: 'center',
     fontWeight: 'bold'
+  },
+
+  // flex box
+  parentFlex: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10,
+  },
+  boxYellow: {
+    width: 70,
+    height: 70,
+    backgroundColor: 'yellow'
+  },
+  boxGreen: {
+    width: 70,
+    height: 70,
+    backgroundColor: 'green'
+  },
+  boxRed: {
+    width: 70,
+    height: 70,
+    backgroundColor: 'red'
   }
+
 })
